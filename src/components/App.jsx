739a89620b2331e5bc56dccdc9015fc9512a62d2
MalgorzataKuszewska <mkuszewska242@gmail.com>
2023-11-11 12:40:01 +0100

    Update hook react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,38 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value.toLowerCase());
+  };
+
+  const addContact = newContact => {
+    newContact.id = nanoid();
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
+
   return (
     <div
       style={{
@@ -6,11 +40,22 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        flexDirection: 'column',
+        fontSize: 24,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Książka kontaktów</h1>
+      <ContactForm onAddContact={addContact} contacts={filteredContacts} />
+
+      <h2>Kontakty</h2>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
     </div>
   );
 };
+
+export default App;
